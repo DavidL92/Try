7a8b76343513402e1106bed1852edd8834fce8e1
wliu <wliu@WS-TAO0099.ad.agi>
2016-03-14 16:33:34 +0800

    update Mediator.cpp

--- Mediator.cpp
+++ Mediator.cpp
@@ -24,12 +24,53 @@ class Mediator
         Mediator(){}
         ~Mediator(){}
         virtual operation(int a){}
-        virtual void register(Colleague * col)
+        virtual void register(int num,Colleague * col)
         {
         }
 };
 
-class ConcreteColleaguaA : public
+class ConcreteMediator : public Mediator
 {
-    
+    public:
+        void operation(int op)
+        {
+        }
+        Colleague* register(int mWho,Colleague *col)
+        {
+            map<int,Colleague*>::const_iterator itr = m_mColleague.find(mWho);
+            if(itr == m_mColleague.end())
+            {
+                m_mColleague.insert(make_pair(mWho,col));
+            }
+        }
+    private:
+        map<int,Colleague *> m_mColleague;
+}
+
+class ConcreteColleaguaA : public Colleague
+{
+    public:
+        void sendmsg(int msg)
+        {
+        }
+        void setMediator(Mediator *)
+        {
+        }
+}
+class ConcreteColleaguaB : public Colleague
+{
+    public:
+        void sendmsg(int msg)
+        {
+        }
+        void setMediator(Mediator *)
+        {
+        }
+}
+
+int main()
+{
+    Mediator * med = new ConcreteMediator();
+    Colleague *col1 = new ConcreteColleaguaA();
+    col1->setMediator(med);
 }
